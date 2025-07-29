62b1c84aa4972381bd1f0f04a26bdbf90e55b8e7
Jestley <deloyxd@gmail.com>
2025-07-29 08:37:24 +0800

    dynamic admin modal, inject html

--- src/js/admin_modal.js
+++
@@ -1,7 +0,0 @@
-async function loadModal() {
-  const response = await fetch('admin_modal.html');
-  const html = await response.text();
-  document.getElementById('admin_modal').innerHTML = html;
-}
-
-loadModal();
