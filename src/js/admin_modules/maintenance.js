bf7fc6803d4c6452af91d8e77a6f4738fcbc5217
Enzo <enzoparanedaniela00@gmail.com>
2025-08-03 17:30:48 +0800

    added: Maintenance but don't have any functions just UI for now

---
+++ src/js/admin_modules/maintenance.js
@@ -0,0 +1,19 @@
+import main from '../admin_main.js';
+
+// default codes:
+let mainBtn, subBtn, sectionTwoMainBtn;
+document.addEventListener('ogfmsiAdminMainLoaded', function () {
+    // change to right sectionName
+    if (main.sharedState.sectionName != 'maintenance') return;
+    mainBtn = document.querySelector(`.section-main-btn[data-section="${main.sharedState.sectionName}"]`);
+    mainBtn.addEventListener('click', mainBtnFunction);
+    subBtn = document.querySelector(`.section-sub-btn[data-section="${main.sharedState.sectionName}"]`);
+    subBtn.classList.remove('hidden');
+    subBtn.addEventListener('click', subBtnFunction);
+    sectionTwoMainBtn = document.getElementById(`${main.sharedState.sectionName}SectionTwoMainBtn`);
+    sectionTwoMainBtn.addEventListener('click', sectionTwoMainBtnFunction);
+    
+    // not default code: sample of custom content setup
+    setupHeader();
+    setupMaintenanceTasks();
+    });
