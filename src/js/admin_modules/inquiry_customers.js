7c3257ed9cbabfd9455719e0d03322eff02435f1
Enzo <enzoparanedaniela00@gmail.com>
2025-10-25 15:24:04 +0800

    fix: monthly

--- src/js/admin_modules/inquiry_customers.js
+++ src/js/admin_modules/inquiry_customers.js
@@ -1305,7 +1305,7 @@ function customerProcessBtnFunction(customer, { firstName, lastName, fullName })
 
     function continueCustomerProcessBtnFunction() {
       main.openModal('yellow', inputs, (result) => {
-        const isMonthlyCustomer = customer.dataset.custom2.toLowerCase().includes('active');
+        const isMonthlyCustomer = customer.dataset.custom4.toLowerCase().includes('days') || customer.dataset.custom2.toLowerCase().includes('active');
         const isPending = customer.dataset.custom2.toLowerCase().includes('pending');
         const priceRate = customer.dataset.custom3.toLowerCase();
         let amount =
