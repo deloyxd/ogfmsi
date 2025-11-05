6148c7b887b57ad575c50e35ddd39e1e225a21d0
deloyxd <deloyxd@gmail.com>
2025-11-06 01:12:40 +0800

    fix: ui admin

--- src/js/admin_modules.js
+++ src/js/admin_modules.js
@@ -8,12 +8,26 @@ scripts.forEach((script) => {
   document.body.appendChild(scriptEl);
 });
 
-VANTA.CLOUDS2({
+// VANTA.CLOUDS2({
+//   el: '#vanta-bg',
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 10.0,
+//   minWidth: 190.0,
+//   texturePath: '/src/images/noise.png',
+// });
+
+VANTA.DOTS({
   el: '#vanta-bg',
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
   minHeight: 10.0,
   minWidth: 190.0,
-  texturePath: '/src/images/noise.png',
+  scale: 1.0,
+  scaleMobile: 1.0,
+  size: 2.3,
+  spacing: 21.0,
+  showLines: false,
 });
